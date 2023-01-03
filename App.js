a64996940292c89aa9326d19492de063932c06f9
Siddharth <tsiddharth7406@gmail.com>
2023-01-03 19:00:52 +0530

    Igniting our app using bundler-parcel

--- App.js
+++ App.js
@@ -1,36 +1,61 @@
-const heading =   React.createElement("h1",{
-    className : "test",
-    id : "h2",
-    key : "k1",
-    style:{
-        color: "blue",
-        textAlign:'center'
-    }
-   }," Namastey Everyone",
-   
-    React.createElement("i",{
+/***!SECTION
+ *
+ * HMR- hot module reloading
+ * File watchers algorithm - C++
+ * Bundling
+ * Minify
+ * Cleaning our code
+ * Dev and Production Build
+ * Super fast build alogorithm
+ * Image Optimization
+ * Caching while development
+ * Compression
+ * Compatable with older versions of browser
+ * HTTPS on dev
+ * Manages the port number
+ * Consisting Hashing Algorithm -
+ * Zero Config
+ *
+ */
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement(
+  "h1",
+  {
+    className: "test",
+    id: "h2",
+    key: "k1",
     style: {
-      color: 'red',
-     }
-   }," to learn React!!"));
+      color: "blue",
+      textAlign: "center",
+    },
+  },
+  " Namastey Everyone"
+);
 
+const heading2 = React.createElement(
+  "h2",
+  {
+    key: "p1",
+    style: {
+      textAlign: "center",
+      fontSize: "1.5rem",
+    },
+  },
+  "from parcel"
+);
 
+console.log("heading-->>", heading);
 
-   const para = React.createElement("p",{  
-    key : "p1",  
-    style:{
-        textAlign: 'center',
-        fontSize: '1.5rem'
-    }
-   },"from zero ... to hero")
+const conatiner = React.createElement(
+  "div",
+  {
+    id: "container",
+  },
+  [heading, heading2]
+);
 
-   
-
-   const conatiner = React.createElement("div",{
-    id:'container'
-   },[heading,para])
-
-   const root = ReactDOM.createRoot(document.getElementById("root"));
-   root.render(conatiner);
-   
-   //console.log(conatiner)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(conatiner);
